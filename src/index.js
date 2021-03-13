36d82ae462dbcae0bfd5a1e739f00644f85b4153
cassio.cappellari@somosphi.com <cassio.cappellari@somosphi.com>
2021-03-13 15:38:39 -0300

    feat: create create account route; install uuid lib

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { 
+    cpf,
+    name,
+  } = req.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    name,
+    cpf,
+    statement: []
+  });
+
+  return res.status(201).send();
+});
+
 
 app.listen(3333);
