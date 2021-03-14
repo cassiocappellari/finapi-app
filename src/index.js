38a5d00acea84580f260e0a963bc54b9913a025b
cassiocappellari <cassiocappellari@gmail.com>
2021-03-14 18:01:34 -0300

    feat: create account route with PUT method in order to update users account data; create get account route in order to show users account data

--- src/index.js
+++ src/index.js
@@ -123,4 +123,19 @@ app.get("/statement/date", (req, res) => {
   return res.json(statement);
 });
 
+app.put("/account", (req, res) => {
+  const { name } = req.body;
+  const { customer } = req;
+
+  customer.name = name;
+
+  return res.status(201).send();
+});
+
+app.get("/account", (req, res) => {
+  const { customer } = req;
+
+  return res.json(customer)
+})
+
 app.listen(3333);
